4432533a172c2bcfc3f2b5a5351b02e3c11ee2fe
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-07 16:12:56 +0000

    more studying for today

---
+++ number_lists.py
@@ -0,0 +1,18 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
+
+print()
+print(len(even))
+print(len(odd))
+#len for strings counts characters. in lists it counts items
+
+print()
+
+print("mississippi".count("s"))#using a string this time testing how count works
+#4 times it found S
+
+
