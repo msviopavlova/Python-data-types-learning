fdedc5a7d49a145ffa970388b2f92008f003654e
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-07 15:08:04 +0000

    Lists/mutables

---
+++ mutable.py
@@ -0,0 +1,40 @@
+shopping_list = ["milk",
+                  "pasta",
+                  "eggs",
+                  "spam",
+                  "bread",
+                  "rice"
+                  ]
+another_list = shopping_list
+print(id(shopping_list))
+print(id(another_list))
+
+shopping_list += ["cookies"] # we have to use square brackets to add it
+print(shopping_list)
+
+print(id(shopping_list)) # the ID remains the same after thanging the list
+
+#python added an item to the existing list without creating a new one.
+print(another_list) #both objects now have cookies added to it, they still are equal
+
+
+a=b=c=d=e=f=another_list # chain assignment
+#we havent copied ne list we just bound a name to it
+
+print(a)
+
+print("Adding cream")
+
+b.append("cream") # appending is adding
+
+print(c)
+print(d)
+
+
+print(f)
+
+#when we look at lists that contain lists multiple variables can be equal/useful
+
+
+
+
