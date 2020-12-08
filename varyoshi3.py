4c4d4f38b67de0a396907bf1c7452b67de901266
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-08 16:06:15 +0000

    switching variables around

---
+++ varyoshi3.py
@@ -0,0 +1,24 @@
+#task: A = “world”
+# B = “hello “
+#
+# Make the value of A be set on variable B.
+# Make the value of B be set on variable A.
+#  my example using 4 variables
+# No hard coding allowed.
+# a = "World"
+# b = "Hello"
+# c = a
+# d = b
+# b= c
+# a = d
+# print(a)
+# print(b)
+# now using 3 variables
+a = "World"
+b = "Hello"
+c = b
+b = a
+a = c
+
+print(a)
+print(b)
